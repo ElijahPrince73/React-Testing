3f9be56fa10d29921c32f3169465d9f7867bacff
Elijah Prince <elijahprince73@gmail.com>
2017-11-13 22:31:23 -0800

    First Test WHOOOOOOHOOOOOO!!!!!!

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,11 +1,15 @@
 import {renderComponent, expect} from '../test_helper';
-import {app} from '../../src/components/app';
+import App from '../../src/components/app';
 
 // Used describe to group together similar test
-describe('App')
+describe('App', () => {
 
-// Use 'it' to test a single attribute of a target
-it('Shows correct text')
+  // Use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
+    //create an instance of app
+    const component = renderComponent(App);
 
-// Use expect to make a 'assertion' about a target
-expect()
+    // Use expect to make a 'assertion' about a target
+    expect(component).to.contain('React simple starter')
+  })
+})
