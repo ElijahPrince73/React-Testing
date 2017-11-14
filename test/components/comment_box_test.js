3ab6c374cefe34866ce65629629cc78784dec56b
Elijah Prince <elijahprince73@gmail.com>
2017-11-14 14:58:52 -0800

    Test Structor setup

---
+++ test/components/comment_box_test.js
@@ -0,0 +1,8 @@
+import {renderComponent, expect} from '../test_helper';
+
+import CommentBox from '../../src/components/comment_box';
+
+describe('CommentBox', () => {
+  it('Has a text area', (done) => {});
+  it('Has a button', (done) => {});
+});
