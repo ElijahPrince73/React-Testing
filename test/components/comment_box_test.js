9148769f53369e5b6fd049479aaa7897e01364c3
elijahprince73 <elijah@1solutionapps.com>
2017-11-16 13:45:46 -0800

    testing controlled inputs

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -1,26 +1,37 @@
-import {renderComponent, expect} from '../test_helper';
-
+import { renderComponent, expect } from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
-
   let component;
 
   beforeEach(() => {
-    component = renderComponent(CommentBox)
+    component = renderComponent(CommentBox);
   });
 
-  it('has the correct class', (done) => {
+  it('has the correct class', () => {
     expect(component).to.have.class('comment-box');
-    done()
   });
 
-  it('Has a text area', (done) => {
+  it('has a text area', () => {
     expect(component.find('textarea')).to.exist;
-    done()
   });
-  it('Has a button', (done) => {
+
+  it('has a button', () => {
     expect(component.find('button')).to.exist;
-    done()
+  });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text in the textarea', () => {
+      expect(component.find('textarea')).to.have.value('new comment');
+    });
+
+    it('when submitted, clears the input', () => {
+      component.simulate('submit');
+      expect(component.find('textarea')).to.have.value('');
+    });
   });
 });
