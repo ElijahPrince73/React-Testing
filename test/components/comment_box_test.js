ddd1b14eacaa35d9b197e9a3e5e5a020c1b943f0
elijahprince73 <elijah@1solutionapps.com>
2017-11-16 09:57:23 -0800

    using before each to condense test

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -3,19 +3,23 @@ import {renderComponent, expect} from '../test_helper';
 import CommentBox from '../../src/components/comment_box';
 
 describe('CommentBox', () => {
+
+  let component;
+
+  beforeEach(() => {
+    component = renderComponent(CommentBox)
+  });
+
   it('has the correct class', (done) => {
-    const component = renderComponent(CommentBox)
-    expect(component).to.have.class ('comment-box');
+    expect(component).to.have.class('comment-box');
     done()
   });
 
   it('Has a text area', (done) => {
-    const component = renderComponent(CommentBox)
     expect(component.find('textarea')).to.exist;
     done()
   });
   it('Has a button', (done) => {
-    const component = renderComponent(CommentBox)
     expect(component.find('button')).to.exist;
     done()
   });
