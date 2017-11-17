2659ecb92a074e8180fcfe5f4ee81aedfb97d905
elijahprince73 <elijah@1solutionapps.com>
2017-11-17 12:10:04 -0800

    testing action creators

---
+++ test/actions/index_test.js
@@ -0,0 +1,18 @@
+import {expect} from '../test_helper';
+import {SAVE_COMMENT} from '../../src/actions/types';
+import {saveComment} from '../../src/actions';
+
+describe('action', () => {
+  describe('saveComment', () => {
+    it('has the correct type', (done) => {
+      const action = saveComment()
+      expect(action.type).to.equal(SAVE_COMMENT)
+      done()
+    });
+    it('has the correct payload', (done) => {
+      const action = saveComment('new comment')
+      expect(action.payload).to.equal('new comment')
+      done()
+    });
+  });
+});
